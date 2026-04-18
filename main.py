fe17b8e8d2c49228a4ab1d76f8e3c75837246264
luizoliveira-art <luiz.oliveira@aluno.ufca.edu.br>
2026-04-18 11:33:33 -0300

    Add dice rolling functionality in main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import random 
+
+inp = input('aperte Enter para rollar o dado')
+if True:
+    rand1 = random.randint(1, 6)
+    rand2 = random.randint(1, 6)
+    print(f'{rand1} {rand2} \n {rand1 + rand2}')
